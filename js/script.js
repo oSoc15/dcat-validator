f10e4c651d0c4d2b4b268ece4650effa4ef78a16
Sébastien Henau <sebastienhenau@hotmail.com>
2015-07-10 09:12:14 +0200

    Added the function to show error messages when fiels have no value and there is now better feedback where the error shows how many syntax mistakes there are in the DCAT feed, it also confirms if the DCAT is valid

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //this makes sure that multiple javascript files can use the same variables
 (function(){
 
-	var feedback, text;
+	var feedback, text, alert, success;
 
 	//init function that gets executed (below) immediatly after all the other functions are loaded
 	function init(){
@@ -49,27 +49,63 @@
  	} 	
 
 	function afterValidate(){
-		var header = $("<h2>");
-		header.text("Errors");
-		$(".errors").append(header);
-		$.each(feedback["errors"], function(key, value){
-			var rowStart = $("<div>", {class:"row start"});
-	    	var start = $("<div>", {class:"col-md-12"});
-		 	var row = $("<div>", {class:"row"});
-		 	var column = $("<div>", {class:"col-sm-12 col-lg-12 col-md-12"});
-		 	var tumbnail = $("<div>", {class:"tumbnail error"});
-		 	var error = $("<p>");
-		 	error.append('<span class="glyphicon glyphicon-remove-sign"></span>');
-		 	error.append(value.error);
+		alert = $("<div>", {class:"alert alert-danger", role:"alert"});
+		success = $("<div>", {class:"alert alert-success", role:"alert"});
+		$(".tab3Textarea").css("border", "1px solid #CCC");
 
-		 	rowStart.append(start);
-		 	start.append(row);
-		 	row.append(column);
-		 	column.append(tumbnail);
-		 	tumbnail.append(error);
+		if($(".errors")){
+			$(".errors").remove();
+		}
+		if($(".alert")){
+			$(".alert-danger").remove();
+			$(".alert-success").remove();
+		}
 
-		 	$(".errors").append(rowStart);
-	    });
+		if($(".tab3Textarea").val() == ""){
+			alert.text("Please fill in the required field by manually inserting your DCAT feed");
+			$(".startContent").prepend(alert);
+			$(".tab3Textarea").css("border", "1px solid #D75452");
+		}else{
+			showFeedback();
+		}
+	}
+
+	function showFeedback(){
+		if(feedback['errors'].length == 0){
+			success.text("Your DCAT - feed is valid. You have no errors or warnings");
+			$(".startContent").prepend(success);
+		}else{
+			if(feedback['errors'].length <= 1){
+				alert.text("Your DCAT - feed is not valid. You have " + feedback['errors'].length + " error. You can find your error below the page.");
+			}else{
+				alert.text("Your DCAT - feed is not valid. You have " + feedback['errors'].length + " errors. You can find your errors below the page.");
+			}
+			$(".startContent").prepend(alert);
+			
+			var errorsContainer = $("<div>", {class:"container errors"});
+			var header = $("<h2>");
+			header.text("Errors");
+			errorsContainer.append(header);
+			$.each(feedback["errors"], function(key, value){
+				var rowStart = $("<div>", {class:"row start"});
+		    	var start = $("<div>", {class:"col-md-12"});
+			 	var row = $("<div>", {class:"row"});
+			 	var column = $("<div>", {class:"col-sm-12 col-lg-12 col-md-12"});
+			 	var tumbnail = $("<div>", {class:"tumbnail error"});
+			 	var error = $("<p>");
+			 	error.append('<span class="glyphicon glyphicon-remove-sign"></span>');
+			 	error.append(value.error);
+
+			 	rowStart.append(start);
+			 	start.append(row);
+			 	row.append(column);
+			 	column.append(tumbnail);
+			 	tumbnail.append(error);
+
+			 	errorsContainer.append(rowStart);
+			 	errorsContainer.insertAfter($(".startContent"));
+		    });
+		}
 	}
 
 	//executing init function after all other functions are loaded
